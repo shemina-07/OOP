9c9098fdf408601b112dfc06097bb2ed493e06c2
shemina-07 <sheminashemina7@gmail.com>
2025-05-05 14:27:40 +0530

    Add files via upload

---
+++ c04/c04/hashmap.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+class hashmap
+{
+public static void main(String args[])
+{
+HashMap<String,Integer> map=new HashMap<>();
+map.put("Germany",4);
+map.put("England",1);
+map.put("Brazil",5);
+System.out.println("Size of map is :"+map.size());
+System.out.println(map);
+map.put("Germany",3);
+System.out.println(map);
+map.remove("Brazil");
+System.out.println(map);
+if(map.containsKey("England"))
+{
+Integer a=map.get("England");
+System.out.println("Value for key"+ "\"England\"is:-"+a);
+}
+}
+}
